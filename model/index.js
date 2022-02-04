955a02cd1d149ce50389609fe3935b4f744b05ba
Frank Garcia <frank.garcia@alegra.com>
2022-02-04 18:05:41 -0300

    entry point for models

---
+++ model/index.js
@@ -0,0 +1,5 @@
+const UserModel = require('./User');
+const PostModel = require('./Post');
+const HobbyModel = require('./Hobby');
+
+module.exports = { User: UserModel, Post: PostModel, Hobby: HobbyModel }
